fbd9d2d1d9605aac17986c9ef13a4e880d20a094
Gabrielle Lepensky <gabrielle.lepensky@gmail.com>
2023-08-07 00:59:15 -0500

    created total monthly

--- client.js
+++ client.js
@@ -10,28 +10,51 @@ function submitForm(event) {
     let idVal = document.querySelector('#idNumber-input').value
     let titleVal = document.querySelector('#title-input').value
     let annualSalaryVal = document.querySelector('#annualSalary-input').value
-    console.log('my vals' , firstNameVal, lastNameVal, idVal, titleVal, annualSalaryVal);
 
-    let employeeTable = document.querySelector('#employeeData');
-
-    let formattedAnnualSalary = new Intl.NumberFormat('en-US', {
-        style: 'currency',
-        currency: 'USD',
-    }).format(annualSalaryVal);
-
-    employeeTable.innerHTML +=`
-    <tr>
-    <td>${firstNameVal}</td>
-    <td>${lastNameVal}</td>
-    <td>${idVal}</td>
-    <td>${titleVal}</td>
-    <td>${formattedAnnualSalary}</td>
-    <td><button onClick="removeRow(event)">Delete</button></td>
-    </tr>   
-    `;
-
-}
-
-function removeRow(event){
-    event.target.closest('tr').remove();
-}
+        // Check if the input for annualSalaryVal is valid
+        if (isNaN(parseFloat(annualSalaryVal))) {
+            alert('Please enter a valid number for Annual Salary.');
+            return;
+        }
+    
+        let employeeTable = document.querySelector('#employeeData');
+    
+        let formattedAnnualSalary = new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'USD',
+        }).format(annualSalaryVal);
+    
+        employeeTable.innerHTML += `
+        <tr>
+        <td>${firstNameVal}</td>
+        <td>${lastNameVal}</td>
+        <td>${idVal}</td>
+        <td>${titleVal}</td>
+        <td>${formattedAnnualSalary}</td>
+        <td><button onClick="removeRow(event)">Delete</button></td>
+        </tr>   
+        `;
+    
+        calculateTotalMonthly();
+    }
+    
+    function removeRow(event) {
+        event.target.closest('tr').remove();
+        calculateTotalMonthly();
+    }
+    
+    function calculateTotalMonthly() {
+        let annualSalaryElements = document.querySelectorAll('#employeeData tbody tr td:nth-child(5)');
+        let totalMonthly = 0;
+    
+        annualSalaryElements.forEach((element) => {
+            let annualSalary = +element.textContent.replace(/[^\d.-]/g, ''); // Remove currency formatting
+            totalMonthly += annualSalary;
+        });
+    
+        let totalMonthlyElement = document.querySelector('#totalMonthlyValue');
+        totalMonthlyElement.textContent = new Intl.NumberFormat('en-US', {
+            style: 'currency',
+            currency: 'USD',
+        }).format(totalMonthly);
+    }
